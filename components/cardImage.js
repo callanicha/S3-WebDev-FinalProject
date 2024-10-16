6e9fa45d67feb03a5bafc136f767f784a366f7f1
lostmart <martinpedraza1979@gmail.com>
2024-10-16 13:09:32 +0200

    card styles and stuff

--- components/cardImage.js
+++ components/cardImage.js
@@ -1,3 +1,5 @@
+import "./cardImage.css"
+
 export const cardImage = () => {
 	const cardDiv = document.createElement("div")
 	cardDiv.classList.add("card-image")
@@ -5,9 +7,9 @@ export const cardImage = () => {
 	const img = new Image()
 	img.src =
 		"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-    img.setAttribute("alt", "avatar image")
-    
-    cardDiv.appendChild(img)
+	img.setAttribute("alt", "avatar image")
+
+	cardDiv.appendChild(img)
 
 	return cardDiv
 }
--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 import { cardComponent } from "./components/cardComponent"
 import { cardImage } from "./components/cardImage"
+import {usersData} from "./components/usersData"
 import "./style.css"
+
+
 
 const imageOne = cardComponent()
 imageOne.appendChild(cardImage())
+imageOne.appendChild(usersData())
 
 app.appendChild(imageOne)
