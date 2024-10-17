a28dd39cfd839845587c45ba436c99664dec6bf0
lostmart <lostmart@hotmail.com>
2024-10-17 17:36:34 +0200

    dynamic images

--- components/cardImage.js
+++ components/cardImage.js
@@ -1,12 +1,15 @@
 import "./cardImage.css"
+/**
+ * @param {string} cardImg // image url to be displayed on the image avatar
+ * @returns
+ */
 
-export const cardImage = () => {
+export const cardImage = (cardImg) => {
 	const cardDiv = document.createElement("div")
 	cardDiv.classList.add("card-image")
 
 	const img = new Image()
-	img.src =
-		"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+	img.src = cardImg || "../assets/blank-profile.webp"
 	img.setAttribute("alt", "avatar image")
 
 	cardDiv.appendChild(img)
